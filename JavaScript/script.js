fa541081b7b669327c4a0c855bc417a0cfbdbc27
Yasseen Ahmanache <yasseen@yasseens-mbp.home>
2020-03-06 23:48:42 +0000

    double slashing complete

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -15,14 +15,30 @@ function myFunction() {
   document.getElementById("prefix").innerHTML = prefix;
 
   //checking to see if it has double slashing in the URL
-  var slashing = str.includes("//");
-  document.getElementById("slashing").innerHTML = slashing;
+  var count = 0;
+
+  for(var i = 0; i < str.length; i++){
+
+    if(str.charAt(i) == '/' && str.charAt(i+1) == '/' && str.charAt(i+2) != '/'){
+      count++;
+    }
+  }
+
+  // assigning the value if the URL contains "@" symbol
+  var slashing_result;
+  if (count > 1){
+    slashing_result = 1;
+  } else {
+    slashing_result = -1;
+  }
+  alert(count);
+  document.getElementById("slashing").innerHTML = slashing_result;
 
   //filterting out the url to contain only an IP address
-  var ip_add
+  var ip_add;
   ip = str.replace(/[^\d.-]/g, '');
   ip2 = ip.replace(/[-]/g, "");
-  alert(ip2);
+  // alert(ip2);
 
   //checking to see whether the number and characters left in the url are in the format of a IP address
   if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip2)
@@ -31,14 +47,14 @@ function myFunction() {
   || /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.\.\.$/.test(ip2)
   || /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.\.\.\.$/.test(ip2)) {
     ip_add = 1;
-    alert("you have an ip address" + ip_add);
+    // alert("you have an ip address" + ip_add);
     document.getElementById("ip_address").innerHTML = ip2;
-    return (true)
+    return (true);
   }
   ip_add = -1;
-  alert("You have entered an invalid IP address!" + ip_add)
+  // alert("You have entered an invalid IP address!" + ip_add)
   document.getElementById("ip_address").innerHTML = "no ip address "  + ip2;
-  return (false)
+  return (false);
 
   //assigning the value of 1 if the Length is greater than 54
   var url_length;
@@ -47,7 +63,7 @@ function myFunction() {
   } else {
     url_length = -1;
   }
-  alert(url_length);
+  // alert(url_length);
 
   //assigning the value if the URL contains "@" symbol
   var at;
@@ -56,7 +72,7 @@ function myFunction() {
   } else {
     at = -1;
   }
-  alert(at);
+  // alert(at);
 
   //assigning the value if the URL contains "@" symbol
   var prefix_result;
@@ -65,14 +81,6 @@ function myFunction() {
   } else {
     prefix_result = -1;
   }
-  alert(prefix_result);
+  // alert(prefix_result);
 
-  //assigning the value if the URL contains "@" symbol
-  var slashing_result;
-  if (str.includes("-")){
-    slashing_result = 1;
-  } else {
-    slashing_result = -1;
-  }
-  alert(slashing_result);
 }
